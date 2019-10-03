ee34fef3ae2a3d396d400c4a2c89ede70f533397
harshjalutharia <harshjalutharia@gmail.com>
2019-10-03 22:11:54 +0530

    Patrolling now works entirely using C++. Yet to delete BP

--- Source/TestingGrounds/ChooseNextWaypoint.cpp
+++ Source/TestingGrounds/ChooseNextWaypoint.cpp
@@ -2,11 +2,25 @@
 
 #include "ChooseNextWaypoint.h"
 #include "BehaviorTree/BlackboardComponent.h"
+#include "GameFramework/Actor.h"
+#include "AIController.h"
+#include "PatrollingGuard.h" // TODO Remove Coupling
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
+	// Get Patrol Points
+	auto ControlledPawn = Cast<APatrollingGuard>(OwnerComp.GetAIOwner()->GetPawn());
+	auto PatrolPoints = ControlledPawn->PatrolPointsCPP;
+
+	// Set next waypoint
 	auto BlackboardComponent = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComponent->GetValueAsInt(IndexKey.SelectedKeyName);
-	UE_LOG(LogTemp, Warning, TEXT("Next Waypoint Index: %i"),Index);
+	BlackboardComponent->SetValueAsObject(NextWaypointKey.SelectedKeyName, PatrolPoints[Index]);
+	// TODO protect against empty array
+
+	// Cycle Index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComponent->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
+
 	return EBTNodeResult::Succeeded;
 }
