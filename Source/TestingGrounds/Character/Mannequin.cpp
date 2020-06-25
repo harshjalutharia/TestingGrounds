196558c085ac8376accf454ed6987643056d9b12
Harsh-Laptop\harsh <harshjalutharia@gmail.com>
2020-06-25 19:17:36 +0530

    Converted BP_Character into Mannequin C++.

---
+++ Source/TestingGrounds/Character/Mannequin.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Mannequin.h"
+#include "Camera/CameraComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "../Weapons/Gun.h"
+
+
+// Sets default values
+AMannequin::AMannequin()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	FPCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FPCamera"));
+	FPCamera->SetupAttachment(RootComponent);
+	FPCamera->bUsePawnControlRotation = true;
+	FPCamera->RelativeLocation = FVector(-39.56f, 1.75f, 64.f);
+
+	FPArms = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("FPArms"));
+	FPArms->SetupAttachment(FPCamera);
+	FPArms->SetOnlyOwnerSee(true);
+	FPArms->bCastDynamicShadow = false;
+	FPArms->CastShadow = false;
+	FPArms->RelativeRotation = FRotator(1.9f, -19.19f, 5.2f);
+	FPArms->RelativeLocation = FVector(-0.5f, -4.4f, -155.7f);
+
+}
+
+// Called when the game starts or when spawned
+void AMannequin::BeginPlay()
+{
+	Super::BeginPlay();
+	if (GunBlueprint == NULL) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Gun blueprint missing."));
+		return;
+	}
+	FPGun = GetWorld()->SpawnActor<AGun>(GunBlueprint);
+	FPGun->AttachToComponent(FPArms, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
+	FPGun->AnimInstance = FPArms->GetAnimInstance();
+}
+
+// Called every frame
+void AMannequin::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMannequin::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void AMannequin::Fire()
+{
+	FPGun->OnFire();
+}
